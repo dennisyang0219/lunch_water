9c0ef710dce6d7a392dffbb1d67eb79edfd524d4
dennisyang0219 <50126657+dennisyang0219@users.noreply.github.com>
2025-09-25 08:48:17 +0800

    Update admin.py

--- lunch/pages/admin.py
+++ lunch/pages/admin.py
@@ -159,7 +159,6 @@ else:
         st.subheader("刪除店家")
         if all_store_names:
             delete_store_index = 0
-            # 只有當 session_state 的值存在且在列表中，才設定索引
             if st.session_state.delete_store_selectbox and st.session_state.delete_store_selectbox in all_store_names:
                 delete_store_index = all_store_names.index(st.session_state.delete_store_selectbox)
             
